729fafd90581272e588ec241527d0d5d16707fbd
Séjœ <jmmwang800@gmail.com>
2025-03-22 22:19:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const session = process.env.SESSION || '';
 const mongoURI = process.env.MONGO_URI || '';
 const mycode = process.env.CODE || '254';
-const botname = process.env.BOTNAME || 'DREADED';
+const botname = process.env.BOTNAME || 'SEJOTECH';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
-const herokuApiKey = process.env.HEROKU_API_KEY || '';
+const herokuApiKey = process.env.HEROKU_API_KEY || 'HRKU-4491fe41-8664-4d8f-9417-41c0c895877c';
 
 module.exports = {
   session,
